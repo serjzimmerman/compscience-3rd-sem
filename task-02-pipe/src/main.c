b0a4219064d05dad7e629ff63ef80ae9aae45bf9
Sergei Zimmerman <serjtsimmerman@gmail.com>
2022-09-26 21:31:50 +0300

    Working task 2

--- task-02-pipe/src/main.c
+++ task-02-pipe/src/main.c
@@ -7,15 +7,14 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#define BUF_SIZE 65536
+
 struct {
   char *input, *output;
   int   input_present, output_present;
-} args;
+} args = {NULL, NULL, 0, 0};
 
-int main(int argc, char *argv[]) {
-  args.input = NULL;
-  args.output = NULL;
-
+void parse_args(int argc, char *argv[]) {
   int opt;
   while ((opt = getopt(argc, argv, "i:o:")) != -1) {
     switch (opt) {
@@ -35,6 +34,10 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Invalid options, you must specify input and output\n");
     exit(1);
   }
+}
+
+int main(int argc, char *argv[]) {
+  parse_args(argc, argv);
 
   int in_fd = open(args.input, O_RDONLY);
   if (in_fd == -1) {
@@ -60,22 +63,29 @@ int main(int argc, char *argv[]) {
   int child;
   if ((child = fork()) == 0) {
     child_full_duplex_pipe(&p);
-    char   write_buf[32];
+    char   write_buf[BUF_SIZE];
     size_t bytes = 0;
 
-    while ((bytes = read_full_duplex_pipe(&p, write_buf, 32)) > 0) {
+    while ((bytes = read_full_duplex_pipe(&p, write_buf, BUF_SIZE)) > 0) {
       write_full_duplex_pipe(&p, write_buf, bytes);
+#ifdef DEBUG_PRINT
+      printf("child: read bytes from parent = %zu\n", bytes);
+#endif
     }
 
+    close_full_duplex_pipe(&p);
+#ifdef DEBUG_PRINT
+    printf("child: exiting\n");
+#endif
     exit(0);
   }
 
   parent_full_duplex_pipe(&p);
-  char   read_buf[32], read_buf_copy[32];
+  char   read_buf[BUF_SIZE];
   size_t currently_read = 0;
 
   while (currently_read < sb.st_size) {
-    size_t read_bytes = read(in_fd, read_buf, 32);
+    size_t read_bytes = read(in_fd, read_buf, BUF_SIZE);
 
     if (read_bytes == -1) {
       perror("Error reading input file: ");
@@ -84,15 +94,22 @@ int main(int argc, char *argv[]) {
 
     currently_read += read_bytes;
     size_t bytes = write_full_duplex_pipe(&p, read_buf, read_bytes);
-
-    size_t from_process_bytes = read_full_duplex_pipe(&p, read_buf_copy, bytes);
-    write(out_fd, read_buf_copy, from_process_bytes);
+    size_t from_process_bytes = read_full_duplex_pipe(&p, read_buf, bytes);
+#ifdef DEBUG_PRINT
+      printf("parent: read bytes from file = %zu\n", read_bytes);
+      printf("parent: wrote bytes to child = %zu\n", bytes);
+#endif
+    write(out_fd, read_buf, from_process_bytes);
   }
+
+  close_full_duplex_pipe(&p);
 
   int exit_code;
   waitpid(child, &exit_code, 0);
 
-  close_full_duplex_pipe(&p);
+#ifdef DEBUG_PRINT
+  printf("parent: child finished with exit code: %d\n", exit_code);
+#endif
 
   close(in_fd);
   close(out_fd);
